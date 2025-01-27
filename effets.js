1852df8c115db178ba6aee092be63ee2fac98bb0
De Carpentier Alban <alban.de-carpentier@student-cs.fr>
2025-01-27 19:37:26 +0100

    Ajout javascript

---
+++ effets.js
@@ -0,0 +1,25 @@
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     document.getElementById("header").style.fontSize = "30px";
+//   } else {
+//     document.getElementById("header").style.fontSize = "90px";
+//   }
+// }
+
+// window.onscroll = function() {
+//     var header = document.getElementsByTagName('header')
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//         header[0].classList.add('header-scroll');
+//     } else {
+//         if (header[0].classList.contains("header-scroll")) {
+//             header[0].classList.remove('header-scroll');
+//         }
+//     }
+//   }
+
+// let header = document.getElementsByTagName('header')
+// header.classList.add('header-scroll')
+console.log("Hello World");
