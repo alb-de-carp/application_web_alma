12608ebf5d2b5ddfeb3ec38da774d05de7ce6d80
De Carpentier Alban <alban.de-carpentier@student-cs.fr>
2025-02-11 13:59:19 +0100

    Travail sur le header pour mobile (sur le javascript)

--- effets.js
+++ effets.js
@@ -2,8 +2,10 @@ window.onscroll = function() {
     var header = document.getElementsByTagName('header');
     var logo1 = document.getElementsByClassName('logo1_alma');
     var logo2 = document.getElementsByClassName('logo2_alma');
-    var menu = document.getElementsByClassName('lien_menu');
     var ul = document.getElementById('ul_menu_deroulant');
+    var image = document.getElementById('img_menu_deroulant');
+    // var menu = document.getElementsByClassName('lien_menu');
+
     if(ul.style.display=="block"){
         ul.style.display="none";
     }
@@ -11,7 +13,10 @@ window.onscroll = function() {
         header[0].classList.add('header-scroll');
         logo1[0].classList.add('logo-alma-scroll');
         logo2[0].classList.add('logo-alma-scroll');
-        menu.classList.add('lien-menu-scroll');
+        image.style.display="none";
+        ul.style.top="50px";
+
+        // menu.classList.add('lien-menu-scroll');
         //  menu.style.color = black;
     } 
     else {
@@ -24,11 +29,14 @@ window.onscroll = function() {
         if (logo2[0].classList.contains("logo-alma-scroll")) {
             logo2[0].classList.remove('logo-alma-scroll');
         }
+        image.style.display="block";
+        ul.style.top="20vh";
+        
         // if (menu.classList.contains("lien-menu-scroll")) {
         //     menu.classList.remove('lien-menu-scroll');
         // }
-     };
-   };
+     }
+   }
 
 window.onload=function(){
     var bouton = document.getElementById('bouton_menu_deroulant');
@@ -41,7 +49,7 @@ window.onload=function(){
         else {
             ul.style.display="block";
         }
-    };
-};
+    }
+}
 
 // console.log("Hello World");
